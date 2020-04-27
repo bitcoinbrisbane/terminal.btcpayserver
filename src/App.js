ab66a75860f4c3be6ccf399fc1b0e3c1127f73ec
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2020-04-28 07:03:03 +1000

    Update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
-import { Button, Col, Container, Row } from 'react-bootstrap';
+import { Button, Col, Container, Row, Modal } from 'react-bootstrap';
 
 import axios from "axios";
 
@@ -8,7 +8,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
-  const [value, setValue] = useState({ total : "0", coin: "btc" });
+  const [value, setValue, show] = useState({ total : "0", coin: "btc", show: false });
 
   const coins = [
     {
@@ -20,6 +20,9 @@ function App() {
   let coin = value.coin;
 
   const handlePress = (e) => {
+
+    console.log(e);
+
     if (value.total === "0") {
       setValue({total: e });
     } else if (value.total === "clear") {
@@ -30,48 +33,51 @@ function App() {
   }
 
   const handleSubmit = (e) => {
-    //Authorization: Basic MFVPUVY3TWlMUFFwWWlIRUkwN1Y3UlFVbEVSeTdWTWluVVcwa0RVSWRJTA==
+
+    console.log("**")
+
+    // //Authorization: Basic MFVPUVY3TWlMUFFwWWlIRUkwN1Y3UlFVbEVSeTdWTWluVVcwa0RVSWRJTA==
     const basicAuth = process.env.REACT_APP_AUTH;
+    const storeID = process.env.REACT_APP_STORE_ID;
     const price = Number(value.total);
 
     const invoice = {
-      "price": price,
+      "price": 1, //price
       "currency": "AUD",
       "orderId": "1234",
       "itemDesc": "POS"
     }
 
-    let storeID = process.env.REACT_APP_STORE_ID;
-
-    var bodyFormData = new FormData();
-    bodyFormData.set('storeId', storeID);
-    bodyFormData.set('price', price);
-    bodyFormData.set('currency', "AUD");
+    
+    const formData = new FormData();
+    formData.set('storeId', storeID);
+    formData.set('price', price);
+    formData.set('currency', "AUD");
 
     axios({
-      method: 'post',
+      method: 'POST',
       url: 'https://payments.bitcoinbrisbane.com.au/api/v1/invoices',
-      data: invoice,
-      headers: { 
-        'Content-Type': 'multipart/form-data',
-        'Authorization Basic': + basicAuth
-      }
+      data: formData
+      // headers: { 
+      //   'Content-Type': 'multipart/form-data',
+      //   'Authorization Basic': + basicAuth
+      // }
       })
       .then(function (response) {
           //handle success
           console.log(response);
-          const invoiceId = response.data.data.id;
-          //return(response);
+          const invoiceId = response.data.id;
+          setValue({ invoiceId: invoiceId, show: true })
       })
       .catch(function (response) {
           //handle error
           console.log(response);
-      });
+          setValue({ show: true })
+    });
   }
 
   return (
     <div className="App">
-
       <header className="App-header">
         <h1>BTC Pay Terminal</h1>
 
@@ -80,7 +86,7 @@ function App() {
         <Container fluid>
           <Row>
             {coins.map(coin => (
-              <Col>
+              <Col key={coin.symbol}>
                 <Button onClick={ e => handlePress("2")} variant="info" block>{coin.symbol}</Button>
               </Col>
             ))}
@@ -112,7 +118,7 @@ function App() {
           <br></br>
           <Row>
             <Col>
-            <Button onClick={ e => handlePress("1")} variant="primary" block>1</Button>
+              <Button onClick={ e => handlePress("1")} variant="primary" block>1</Button>
             </Col>
             <Col>
               <Button onClick={ e => handlePress("2")} variant="primary" block>2</Button>
@@ -137,20 +143,27 @@ function App() {
           <Row>
             <Col></Col>
             <Col>
-              <Button onClick={ e => handleSubmit()} variant="success" block>BUY</Button>
+              <Button onClick={ e => handleSubmit("buy")} variant="success" block>BUY</Button>
             </Col>
             <Col></Col>
           </Row>
         </Container>
       </header>
 
-      <form method="POST" action="https://payments.bitcoinbrisbane.com.au/api/v1/invoices" className="btcpay-form btcpay-form--block">
+      {/* <Modal show={show}>
+        <Modal.Header>
+            <Modal.Title>Pay</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+      </Modal> */}
+
+      {/* <form method="POST" action="https://payments.bitcoinbrisbane.com.au/api/v1/invoices" className="btcpay-form btcpay-form--block">
         <input type="hidden" name="storeId" value="J6kKcRQmyREYhBvhid9NvrgcqQksxZTTHtUANkKTRLqb" />
         <input type="hidden" name="checkoutDesc" value="Order" />
         <input type="hidden" name="price" value="10" />
         <input type="hidden" name="currency" value="AUD" />
         <input type="image" className="submit" name="submit" src="https://payments.bitcoinbrisbane.com.au/img/paybutton/pay.svg" alt="Pay with BtcPay, Self-Hosted Bitcoin Payment Processor"/>
-      </form>
+      </form> */}
     </div>
   );
 }
